b353d235fbcb93cf6a4b6076772764eb5140701b
汪瑶 <wangyao6@wangyaodeMacBook-Pro.local>
2017-01-06 18:24:12 +0800

    up

--- src/main/java/com/letv/emm/controller/LeMobileAuth.java
+++ src/main/java/com/letv/emm/controller/LeMobileAuth.java
@@ -2,15 +2,8 @@ package com.letv.emm.controller;
 
 import com.letv.emm.services.AuthService;
 import com.letv.emm.vo.AppVo;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.io.IOException;
 
 @RestController
 @CrossOrigin
