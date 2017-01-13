7c3b8df10d4bb401af846d3a8453ffdbab7b9200
wangyao6 <Aa123456>
2017-01-13 10:57:15 +0800

    finished derby db

--- src/main/java/com/letv/emm/entity/PushPropEntity.java
+++ src/main/java/com/letv/emm/entity/PushPropEntity.java
@@ -1,11 +1,27 @@
 package com.letv.emm.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Table(name = "t_push_prop")
+@Entity
 public class PushPropEntity {
+    private long id;
+
     private String pub;
 
+    @Id
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name="BIRTH",nullable=false)
     public String getPub() {
         return pub;
     }
