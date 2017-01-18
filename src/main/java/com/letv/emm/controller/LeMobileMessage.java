cc3f2d7b25d5cca353ce74fb128abe76aa5b482f
wangyao6 <Aa123456>
2017-01-18 18:12:50 +0800

    消息推送

--- src/main/java/com/letv/emm/controller/LeMobileMessage.java
+++ src/main/java/com/letv/emm/controller/LeMobileMessage.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
-@RequestMapping(path = "/LeMobile/send")
+@RequestMapping(path = "/LeMobile/api/send")
 public class LeMobileMessage {
     @Autowired
     private MessageService messageService;
