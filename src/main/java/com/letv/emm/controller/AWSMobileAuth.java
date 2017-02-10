225be33ea44bf9afec2feadfdafeae9919301436
wangyao6 <Aa123456>
2017-02-10 10:08:26 +0800

    aws auth

---
+++ src/main/java/com/letv/emm/controller/AWSMobileAuth.java
@@ -0,0 +1,92 @@
+package com.letv.emm.controller;
+
+import com.letv.emm.services.AuthService;
+import com.letv.emm.vo.AppVo;
+import com.letv.emm.vo.StatusCode;
+import com.letv.emm.vo.StatusVo;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@RestController
+@CrossOrigin
+@RequestMapping(path = "/LeMobile/aws/api")
+@ConfigurationProperties(prefix="spring.aws.mobile")
+public class AWSMobileAuth {
+    @Autowired
+    private AuthService authService;
+
+    private String authUrl;
+
+    private String appId;
+
+    private String appSecret;
+
+    private String grantType;
+
+    @RequestMapping(value = "/auth/{ticket}", method = RequestMethod.POST)
+    @ResponseBody
+    public StatusVo auth(@PathVariable("ticket") String ticket, HttpServletRequest request) {
+        StatusVo statusVo = new StatusVo();
+        AppVo app = new AppVo();
+        app.setAppId(appId);
+        app.setAppSecret(grantType);
+        app.setAppSecret(appSecret);
+        String accessToken = authService.getAccessToken(app);
+        if (null == accessToken || accessToken.isEmpty()) {
+            statusVo.setCode(StatusCode.FATAL.ordinal());
+            statusVo.setMessage("Ticket auth failed!");
+            return statusVo;
+        }
+        String author = authService.getUserInfo(ticket, accessToken);
+        JSONObject authorJson = new JSONObject(author);
+        String mobile = authorJson.getString("mobile");
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(authUrl)
+                .append("?userId=")
+                .append(mobile)
+                .append("&pwd=pwd&bip=")
+                .append(request.getRemoteHost())
+                .append("&lang=cn");
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost sidPost = new HttpPost(stringBuilder.toString());
+        String sid = null;
+        try {
+            CloseableHttpResponse response = httpClient.execute(sidPost);
+            sid = EntityUtils.toString(response.getEntity());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        statusVo.setCode(StatusCode.SUCCESS.ordinal());
+
+        JSONObject sidObj = new JSONObject();
+        sidObj.put("sid", sid);
+        statusVo.setBody(sidObj);
+        return statusVo;
+    }
+
+    public void setAuthUrl(String authUrl) {
+        this.authUrl = authUrl;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public void setAppSecret(String appSecret) {
+        this.appSecret = appSecret;
+    }
+
+    public void setGrantType(String grantType) {
+        this.grantType = grantType;
+    }
+}
